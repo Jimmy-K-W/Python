2cc88db4e444f1e9ba7d9babb3df895062eab272
Jimmy-K-W <wainainakimani2012@gmail.com>
2023-11-02 10:56:21 +0300

    print the python script that accepts user input and prints it on the console

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Create a python script that accepts user input and prints the results on the console
+
+# Create a variable that will store the user's input
+
+username = input("Add your username: ")
+    
+print(username:)
